61c1a589ea1129d5342598335f565da70086e8bd
sankiago <lpzsds@gmail.com>
2025-04-11 06:54:57 -0500

    bulk keyword processing

--- news_structurer.py
+++ news_structurer.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 import pandas as pd
 from dotenv import load_dotenv
 import concurrent.futures
+from testing import format_json 
 
 #importando api key
 load_dotenv()
@@ -16,7 +17,7 @@ client = OpenAI(
 )
 
 # Carga el archivo Excel
-archivo_excel = "./resultado_pipelines.xlsx"  # Cambia esto por la ruta a tu archivo Excel
+archivo_excel = "./scrap_diccionario.xlsx"  # Cambia esto por la ruta a tu archivo Excel
 df = pd.read_excel(archivo_excel)
 
 # Itera sobre las entradas y utiliza la API
@@ -35,32 +36,34 @@ def estructurar_noticia(prompt):
 
 prompt_base = ""
 
-with open("prompt_final.txt", "r", encoding="utf-8") as archivo:  # Cambia "archivo.txt" por la ruta a tu archivo
+with open("./prompts/prompt_simple.txt", "r", encoding="utf-8") as archivo:  # Cambia "archivo.txt" por la ruta a tu archivo
     prompt_base = archivo.read()  # Lee todo el contenido del arch
 
 # Concatenar datos para la entrada
-df['Entrada'] = "Título: "+ df['title'] + "\n" + df['content']
+df['Entrada'] = "Título: "+ df['title'] + "\n autor: " + df['author'] + "\n Fecha:" + df['date'] + "\n url:"+ df['url'] + "\n Keyword: " + df['keyword'] + '\n' + df['content']
 
 # Itera y procesa cada fila
-# resultados = []
-# for idx, entrada in enumerate(df['Entrada']):  # Cambia 'Columna' por el nombre de la columna que contiene las entradas
-#     print(f'Estructurando noticia {idx+1}/{len(df)}')
-#     prompt = f"{prompt_base}\n{entrada}"
-#     resultado = estructurar_noticia(prompt)
-#     resultados.append(resultado)
-#     print(f'Noticia {idx+1}/{len(df)} estructurada')
-
+resultados = []
 prompts = prompt_base + "\n" + df['Entrada']
+# prompts = prompts[-1:]
+prompts = prompts[:30]
+
+for idx, prompt in enumerate(prompts):  # Cambia 'Columna' por el nombre de la columna que contiene las entradas
+    print(f'Estructurando noticia {idx+1}/{len(prompts)}')
+    resultado = estructurar_noticia(prompt)
+    resultados.append(resultado)
+    print(f'Noticia {idx+1}/{len(prompts)} estructurada')
+
 # prompts = [f"{prompt_base}\n{entrada}" for entrada in df['Entrada']]
-# prompts = [prompts[0]]
 
 # Using ThreadPoolExecutor to make parallel requests
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = list(executor.map(estructurar_noticia, prompts))
+# with concurrent.futures.ThreadPoolExecutor() as executor:
+#     results = list(executor.map(estructurar_noticia, prompts))
+format_json(resultados)
 
-# Agrega los resultados de vuelta al dataframe
-df['Resultados'] = results
+# # Agrega los resultados de vuelta al dataframe
+# df['Resultados'] = results
 
-# print(results)
-# Guarda los resultados en un nuevo archivo Excel
-df.to_excel("noticias_estructuradas2.xlsx", index=False)
+# # print(results)
+# # Guarda los resultados en un nuevo archivo Excel
+# df.to_excel("noticias_keywords.xlsx", index=False)
